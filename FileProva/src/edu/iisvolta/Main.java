885578ec91eb66ac757570203a59e37c05b89cce
EdoardoRubechi <rubechi.@iisvolta.it>
2023-09-25 20:52:44 +0200

    Modifica con lettura delle stringhe in singole parti

--- FileProva/src/edu/iisvolta/Main.java
+++ FileProva/src/edu/iisvolta/Main.java
@@ -12,11 +12,11 @@ public class Main {
 	public static void main(String[] args) {
 		//***** SCFRITTURA SU UN FILE *****
 		try {
-			FileWriter fw =new FileWriter("prodotti.txt");//il file viene creato nella stessa cartella del progetto
-			fw.write("Pane\n"); // ogni isctruzione scrive una riga nel file
-			fw.write("Pasta\n");
-			fw.write("Biscotti\n");
-			fw.write("Salame\n");
+			FileWriter fw =new FileWriter("prodotti.csv");//il file viene creato nella stessa cartella del progetto
+			fw.write("Pane;3.50;20\n"); // ogni isctruzione scrive una riga nel file
+			fw.write("Pasta;1.60;45\n");
+			fw.write("Biscotti;4.70;80\n");
+			fw.write("Salame;35;37\n");
 			fw.close(); 		// ricordarsi sempre di chiudere il file
 			System.out.println("File correttamente creato"); 
 		} catch (IOException e) { //subentra solo in caso di eccezione (errore)
@@ -25,13 +25,19 @@ public class Main {
 		}
 
 		//***** LEGGERE DA UN FILE *****
-		File f= new File ("Prodotti.txt");
+		File f= new File ("Prodotti.csv");
+		String[] dati;  //si crea un vettore
 		try {
 			Scanner sc = new Scanner (f); // questo tipo di scanner legge direttamente dal file una riga alla volta senza inserire manualmente gli input
 			String riga;
 			while (sc.hasNextLine()) { //hasNextLine = Funzione Booleana. Ripeti finché ci sono altre righe da leggere
 				riga = sc.nextLine();  //Leggi una riga dal file
-				System.out.println("Trovato prodotto: "+riga);
+				dati=riga.split(";"); //divide la riga usando il carattere nelle parentesi come separatore
+				System.out.println("Trovato prodotto: ");
+				System.out.println("        Nome:     "+dati[0]);
+				System.out.println("        Prezzo:   "+dati[1]);
+				System.out.println("        Quantità: "+dati[2]);
+				System.out.println();
 			}
 			sc.close(); // Chiudi il file
 			System.out.println("\n\nLettura terminata.");
